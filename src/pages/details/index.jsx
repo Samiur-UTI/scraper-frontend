7d33ed22432272351b487efb0bea006d068ab14f
Samiur-UTI <samiurkhan94@gmail.com>
2022-03-24 22:41:13 +0600

    map link added

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,88 +1,94 @@
-import React,{useState,useEffect} from 'react'
-import {useLocation} from 'react-router-dom'
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import Container from '@mui/material/Container';
+import React, { useState, useEffect } from 'react'
+import { useLocation,useNavigate } from 'react-router-dom'
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import Carousel from 'react-material-ui-carousel'
+import Link from '@mui/material/Link';
 import axios from 'axios'
+import './index.css'
 export default function Details() {
-  const {state} = useLocation()
-  const [details,setDetails] = useState('')
-  async function fetchDetails(){
-    const responseData = await axios.get(`http://localhost:8000/details`,{params:state})
+  const { state } = useLocation()
+  const [details, setDetails] = useState('')
+  async function fetchDetails() {
+    const responseData = await axios.get(`http://localhost:8000/details`, { params: state })
     return responseData.data
   }
-  useEffect(()=>{
-    fetchDetails().then(data=>{
-      if(data.success){
+  const navigate = useNavigate()
+  useEffect(() => {
+    fetchDetails().then(data => {
+      if (data.success) {
         setDetails(data.data)
-        // console.log("Details",data.data)
+        console.log("Details", data.data)
       }
-    }).catch(err=>{
+    }).catch(err => {
       console.log(err)
     })
-  },[])
-  const itemData = details.photo ? JSON.parse(details.photo): []
+  }, [])
+  const itemData = details.photo ? JSON.parse(details.photo) : []
   // console.log("PHOTOS",itemData)
-  if(!details){
+  if (!details) {
     return <div>Loading...</div>
   }
-  return(
+  return (
     <Container>
-      <ImageList sx={{ width: 500, height: 450 }} cols={1} rowHeight={100}>
+      <Carousel className='carousel'>
         {itemData.length ? itemData.map((item) => (
-          <ImageListItem key={item.img}>
-            <img
-              src={`${item}`}
-              srcSet={`${item}`}
-              loading="lazy"
-            />
-          </ImageListItem>
-        )): <div>Loading...</div>}
-      </ImageList>
+          <img
+            alt="The house from the offer."
+            src={item}
+            loading='lazy'
+            className='carousel-img'
+          />
+
+        )) : <div>Loading...</div>}
+      </Carousel>
       <Box sx={{ width: 500, height: 450 }}>
-      <List>
+        <List>
           <ListItem disablePadding>
-              <ListItemText primary="Property Name" />
-              <ListItemText primary={details.name} />
+            <ListItemText primary="Property Name" />
+            <ListItemText secondary={details.name} />
           </ListItem>
           <ListItem disablePadding>
-              <ListItemText primary="Address"/>
-              <ListItemText primary={details.property_address} />
+            <ListItemText primary="Address" />
+            <ListItemText secondary={details.property_address} />
           </ListItem>
           <ListItem disablePadding>
-              <ListItemText primary="City"/>
-              <ListItemText primary={details.city} />
+            <ListItemText primary="City" />
+            <ListItemText secondary={details.city} />
           </ListItem>
           <ListItem disablePadding>
-              <ListItemText primary="State"/>
-              <ListItemText primary={details.state} />
+            <ListItemText primary="State" />
+            <ListItemText secondary={details.state} />
           </ListItem>
           <ListItem disablePadding>
-              <ListItemText primary="Phone"/>
-              <ListItemText primary={details.phone} />
+            <ListItemText primary="Phone" />
+            <ListItemText secondary={details.phone} />
           </ListItem>
           <ListItem disablePadding>
-              <ListItemText primary="Type"/>
-              <ListItemText primary={details.property_type} />
+            <ListItemText primary="Type" />
+            <ListItemText secondary={details.property_type} />
           </ListItem>
           <ListItem disablePadding>
-              <ListItemText primary="County"/>
-              <ListItemText primary={details.county} />
+            <ListItemText primary="County" />
+            <ListItemText secondary={details.county} />
           </ListItem>
           <ListItem disablePadding>
-              <ListItemText primary="Zip Code"/>
-              <ListItemText primary={details.zip} />
+            <ListItemText primary="Zip Code" />
+            <ListItemText secondary={details.zip} />
           </ListItem>
           <ListItem disablePadding>
-              <ListItemText primary="Capacity"/>
-              <ListItemText primary={details.capacity} />
+            <ListItemText primary="Capacity" />
+            <ListItemText secondary={details.capacity} />
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemText primary="Map Link" />
+            <ListItemText secondary={details.map ? <Link href={`${details.map}`}>View Location</Link> : 'No location available'} />
           </ListItem>
         </List>
-      </Box>     
+      </Box>
     </Container>
   )
 }
